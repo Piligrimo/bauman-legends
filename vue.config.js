0b5bc8eab82d01236819c0c9c95b66f51b299e75
Максим Овчинников <msovchi6@mts.ru>
2020-09-18 23:46:16 +0300

    https for dev server

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const fs = require('fs')
+
 module.exports = {
-    publicPath: '/'
+    publicPath: '/',
+    devServer: {
+      https: {
+        key: fs.readFileSync('./certs/example.com+5-key.pem'),
+        cert: fs.readFileSync('./certs/example.com+5.pem'),
+      },
+      public: 'https://localhost:8080/'
   }
+}
