f221f44da8235c10475cde41b8256539ed56294a
Максим Овчинников <msovchi6@mts.ru>
2020-09-19 16:23:01 +0300

    https enabled

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-// const fs = require('fs')
+ const fs = require('fs')
 
 module.exports = {
     publicPath: '/',
-  //   devServer: {
-  //     https: {
-  //       key: fs.readFileSync('./certs/example.com+5-key.pem'),
-  //       cert: fs.readFileSync('./certs/example.com+5.pem'),
-  //     },
-  //     public: 'https://localhost:8080/'
-  // }
+    devServer: {
+      https: {
+        key: fs.readFileSync('./certs/example.com+5-key.pem'),
+        cert: fs.readFileSync('./certs/example.com+5.pem'),
+      },
+      public: 'https://localhost:8080/'
+  }
 }
