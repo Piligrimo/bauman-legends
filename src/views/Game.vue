25462aa8282e596003295fda1f7c6b8979c72432
Максим Овчинников <msovchi6@mts.ru>
2020-09-30 15:52:03 +0300

    timer added

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -4,17 +4,25 @@
       <div v-loading="task === null">
         <h3 class="layout__title">Игра</h3>
         <p v-if="!isPlay && task">{{task.text}}</p>
-        <p v-if="isSuccess">Вы ответили верно! Скажи своим товарищам по команде обновить страницу на своих устройствах. Возьмите следующее задание, когда будете готовы</p>
+
         <div v-if="html" v-html="html" class="task-view"/>
+        
+        <h3>На решение осталось {{getMinutes}}:{{getSeconds}}</h3>
+
+        <p v-if="isSuccess">Вы ответили верно! Скажи своим товарищам по команде обновить страницу на своих устройствах. Пусть капитан возьмет следующее задание, когда будете готовы</p>
+        <p v-if="isFail">К сожалению вы не смогли ответить верно :( Пусть капитан возьмет следующее задание, когда будете готовы</p>
+        
         <el-input v-if="isPlay" class="layout__item" placeholder="Oтвет" v-model="answer"/>
+
         <el-button v-if="isCaptain && isPause" type="primary" class="layout__item" @click="next">Взять задание</el-button>
         <el-button v-if="isPlay" type="primary" class="layout__item" @click="answerToTask">Ответить</el-button>
+       
         <div v-if="isCaptain && isPlay" class="layout__actions">
           <el-button class="layout__action" type="primary" @click="skip">Пропустить задание</el-button>
           <el-button :style="{'margin-left': 0}" class="layout__action" type="primary" @click="hint">Взять подсказку</el-button>
         </div>
+
         <p class="error-message" v-if="errorMessage">{{errorMessage}}</p>
-        
       </div>
     </div>
   </div>
@@ -29,12 +37,23 @@ export default {
     return {
       task: null,
       errorMessage: '',
-      answer: ''
+      answer: '',
+      timeOnPageLoad: 0,
+      timeRemaining: 0,
+      timer: null
     }
   },
   async created () {
     const {data} = await getTask() 
     this.task = data
+    this.timeOnPageLoad = Number(new Date()) / 1000
+    if (this.isPlay && this.task?.task?.task_type === 'MAIN') {
+      this.timeRemaining = this.timeRemainedOnPageLoading
+      const vue = this
+      this.timer = setInterval(() => {
+        vue.timeRemaining-- 
+      }, 1000)
+    }
   },
   store,
   computed:{
@@ -53,8 +72,24 @@ export default {
     isSuccess () {
       return this.task?.task?.task_status === 'SUCCESS'
     },
+    isFail () {
+      return this.task?.task?.task_status === 'FAIL'
+    },
     html () {
       return this.task?.task?.html
+    },
+    deadlineTime () {
+      return this.task?.task?.start_time + this.task?.task?.duration
+    },
+    timeRemainedOnPageLoading () {
+      return this.deadlineTime - this.timeOnPageLoad
+    },
+    getMinutes () {
+      return Math.floor(this.timeRemaining / 60)
+    },
+    getSeconds () {
+      const seconds = Math.floor(this.timeRemaining % 60)
+      return seconds > 10 ? seconds : '0' + seconds
     }
   },
   watch: {
@@ -90,6 +125,9 @@ export default {
       } catch (e) {
         this.errorMessage = e.response.data.message
       }
+    },
+    async hint () {
+
     }
   }
 }
@@ -111,11 +149,6 @@ export default {
 
 .game-bg-3 {
   background-image: url('../assets/game-bg-3.jpg');
-}
-
-.task-view {
-   max-height: 300px;
-  overflow: hidden scroll;
 }
 
 </style>
